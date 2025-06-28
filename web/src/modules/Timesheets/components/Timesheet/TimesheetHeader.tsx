ea04228cae93f10d10fa5baafa63ae69053710d5
DanielSerio <dserio.d@gmail.com>
2025-06-28 11:37:22 -0400

    added spot for name and save button

--- web/src/modules/Timesheets/components/Timesheet/TimesheetHeader.tsx
+++ web/src/modules/Timesheets/components/Timesheet/TimesheetHeader.tsx
@@ -1,11 +1,27 @@
-import { Anchor, Flex } from "@mantine/core";
+import { Anchor, Button, Flex, Group, Text } from "@mantine/core";
+import { TbDeviceFloppy } from "react-icons/tb";
 
 export function TimesheetHeader({}: { isLoading?: boolean }) {
   return (
-    <Flex className="timesheet-header" align="center" px="sm" h={48}>
+    <Flex
+      className="timesheet-header"
+      align="center"
+      justify="space-between"
+      px="sm"
+      h={48}
+    >
       <Anchor href="/timesheets">
         <span>&lt;&nbsp;Back To Timesheets</span>
       </Anchor>
+      <Group>
+        <Text
+          contentEditable
+          dangerouslySetInnerHTML={{ __html: "Timesheet 2025/06/28" }}
+        />
+        <Button size="xs" rightSection={<TbDeviceFloppy />}>
+          <Text>Save</Text>
+        </Button>
+      </Group>
     </Flex>
   );
 }
